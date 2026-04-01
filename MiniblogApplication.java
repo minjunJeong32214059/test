50fcc46dd3a372e528ca978594cf123dfcc0669b
JeongMinJun <jj353535jj@gmail.com>
2026-04-01 19:51:24 +0900

    feat: VM 생성 API 구현
    feat: VM 생성 API 구현 (#3 )

--- MiniblogApplication.java
+++ MiniblogApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MiniblogApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MiniblogApplication.class, args);
+		SpringApplication.run(MiniblogApplicationㅈㄷㄹ.class, args);
 	}
 ㅈㄷㄹㅈㄷㄹㄷㅈ
 }ㄷㅈㄹㄷㅈㄷㄹㄷㅈㄹㄷㅈㄹㅊ
